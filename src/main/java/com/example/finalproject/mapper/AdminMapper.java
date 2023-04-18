c3bbb2c933531bded997139feac521b1b8036244
r.shahroudi <roya.shahroudi20@gmail.com>
2023-04-18 18:02:48 +0330

    feat: add admin mapper

---
+++ src/main/java/com/example/finalproject/mapper/AdminMapper.java
@@ -0,0 +1,27 @@
+package com.example.finalproject.mapper;
+
+import com.example.finalproject.domain.Admin;
+import com.example.finalproject.dto.AdminDTO;
+import org.springframework.stereotype.Component;
+
+import java.util.Objects;
+
+@Component
+public class AdminMapper {
+    public Admin toEntity(AdminDTO adminDTO) {
+        if (Objects.isNull(adminDTO)) return null;
+        return new Admin()
+                .setId(adminDTO.getId())
+                .setUsername(adminDTO.getUsername())
+                .setPassword(adminDTO.getPassword());
+    }
+
+    public AdminDTO toDTO(Admin admin) {
+        if (Objects.isNull(admin)) return null;
+        return new AdminDTO()
+                .setId(admin.getId())
+                .setUsername(admin.getUsername())
+                .setPassword(admin.getPassword());
+    }
+
+}
