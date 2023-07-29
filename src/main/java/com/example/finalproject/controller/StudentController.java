dfb0b38b4a2726c224fb95f9dd78a466e2de6265
r.shahroudi <roya.shahroudi20@gmail.com>
2023-07-29 20:53:36 +0330

    feat: add token to login and register headers

--- src/main/java/com/example/finalproject/controller/StudentController.java
+++ src/main/java/com/example/finalproject/controller/StudentController.java
@@ -2,8 +2,10 @@ package com.example.finalproject.controller;
 
 import com.example.finalproject.dto.StudentDTO;
 import com.example.finalproject.service.IStudentService;
+import com.example.finalproject.utils.JwtUtil;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -18,13 +20,22 @@ public class StudentController {
     @Autowired
     private IStudentService studentService;
 
+    @Autowired
+    private JwtUtil jwtUtil;
+
     @PostMapping("/login")
-    public ResponseEntity<StudentDTO> login(@RequestBody @Valid StudentDTO student) {
-        return new ResponseEntity<>(studentService.login(student), HttpStatus.OK);
+    public ResponseEntity<StudentDTO> login(@RequestBody @Valid StudentDTO studentDTO) {
+        StudentDTO student =  studentService.login(studentDTO);
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add(HttpHeaders.AUTHORIZATION, jwtUtil.generateAccessToken(student));
+        return new ResponseEntity<>(student, httpHeaders, HttpStatus.OK);
     }
 
     @PostMapping("/register")
-    public ResponseEntity<StudentDTO> register(@RequestBody @Valid StudentDTO student) {
-        return new ResponseEntity<>(studentService.register(student), HttpStatus.CREATED);
+    public ResponseEntity<StudentDTO> register(@RequestBody @Valid StudentDTO studentDTO) {
+        StudentDTO student =  studentService.register(studentDTO);
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add(HttpHeaders.AUTHORIZATION, jwtUtil.generateAccessToken(student));
+        return new ResponseEntity<>(student, httpHeaders, HttpStatus.CREATED);
     }
 }
