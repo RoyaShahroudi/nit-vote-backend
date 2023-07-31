68caa86dfca5cb931bd657faa684c7ffd2739f92
r.shahroudi <roya.shahroudi20@gmail.com>
2023-07-31 22:49:11 +0330

    feat: add findAllByElectionIdAndCandidateId

--- src/main/java/com/example/finalproject/repository/CandidateGroupRepository.java
+++ src/main/java/com/example/finalproject/repository/CandidateGroupRepository.java
@@ -10,4 +10,6 @@ import java.util.List;
 @Repository
 public interface CandidateGroupRepository extends JpaRepository<CandidateGroup, Integer> {
     List<CandidateGroup> findAllByElectionId(int electionId);
+
+    List<CandidateGroup> findAllByElectionIdAndCandidateId(int electionId, int candidateId);
 }
