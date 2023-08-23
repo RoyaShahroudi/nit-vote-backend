88fb4b4a81edab7370bff5dcc5088d5c3fa8a099
r.shahroudi <roya.shahroudi20@gmail.com>
2023-08-23 20:11:20 +0330

    fix: CORS error

--- src/main/java/com/example/finalproject/config/SecurityConfig.java
+++ src/main/java/com/example/finalproject/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.finalproject.config;
 import jakarta.servlet.DispatcherType;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -12,6 +13,11 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -41,14 +47,26 @@ public class SecurityConfig {
     }
 
     @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("authorization", "content-type", "x-auth-token", "*", "**"));
+        configuration.setExposedHeaders(List.of("x-auth-token", "*", "**"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        http.csrf().disable().headers().frameOptions().disable().and()
+        http.csrf().disable().cors().and().headers().frameOptions().disable().and()
                 .authorizeHttpRequests()
                 .requestMatchers("/v1/student/register").permitAll()
                 .requestMatchers("/v1/student/login").permitAll()
                 .requestMatchers("/v1/student/*").hasRole("STUDENT")
-                .requestMatchers("/v1/admin/submit").permitAll()
+                .requestMatchers("/v1/admin/submit").hasRole("ADMIN")
                 .requestMatchers("/v1/admin/login").permitAll()
                 .requestMatchers("/v1/admin/*").hasRole("ADMIN")
                 .requestMatchers("/v1/election/new").hasRole("ADMIN")
@@ -58,11 +76,13 @@ public class SecurityConfig {
                 .requestMatchers("/v1/election/result/*").hasRole("ADMIN")
                 .requestMatchers("/v1/election/student/result/*").hasRole("STUDENT")
                 .requestMatchers("/v1/election/get-candidates-for-students/*").hasRole("STUDENT")
+                .requestMatchers("/v1/election/get-elections-history-for-student").hasRole("STUDENT")
                 .requestMatchers("/v1/election/in-progress").hasRole("ADMIN")
                 .requestMatchers("/v1/candidate/submit").hasRole("ADMIN")
                 .requestMatchers("/v1/candidate/by-election-id/*").hasRole("ADMIN")
                 .requestMatchers("/v1/candidate/get-all").hasRole("ADMIN")
                 .requestMatchers("/v1/vote/submit").hasRole("STUDENT")
+                .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                 .dispatcherTypeMatchers( DispatcherType.ERROR ).permitAll()
                 .and()
                 .httpBasic()
